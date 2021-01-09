3e2638c3ab0bcdf3d0c487d956dce84b98253c75
gmt061 <62307389+gmt061@users.noreply.github.com>
2021-01-09 21:07:21 +0300

    First Commit

---
+++ precache-manifest.c36005caa19da4349349ed46dd136dcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bce7046c45d72b2b470",
+    "url": "/css/app.2e19e752.css"
+  },
+  {
+    "revision": "db4b56af30cac7320b84",
+    "url": "/css/chunk-vendors.dcd8433d.css"
+  },
+  {
+    "revision": "6be79b47e7cc7275d43698c5346d73d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bce7046c45d72b2b470",
+    "url": "/js/app.82d8c62b.js"
+  },
+  {
+    "revision": "db4b56af30cac7320b84",
+    "url": "/js/chunk-vendors.9f31779c.js"
+  },
+  {
+    "revision": "ffa1ed858ab71257554bc4d4f3e07d7b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
